832452b92956e968fd712fea5c2c100c98d3da76
Sahil Ohe <sahilohe97@gmail.com>
2020-10-10 08:29:47 +0530

    Create Heron_Formula.java

---
+++ Heron_Formula.java
@@ -0,0 +1,25 @@
+package com.fsociety97;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Heron_Formula {
+
+    public static void main(String[] args) {
+        //Creating a instance of Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Asking the use for three sides
+        System.out.println("Enter the sides:");
+        double firstSide = input.nextDouble();
+        double secondSide = input.nextDouble();
+        double thirdSide = input.nextDouble();
+
+        //finding the semi-perimeter and area
+        double semiPerimeter = (firstSide + secondSide + thirdSide) / 2;
+        double answer = Math.sqrt(semiPerimeter * (semiPerimeter - firstSide) * (semiPerimeter - secondSide) * (semiPerimeter - thirdSide));
+
+        System.out.println("The answer is " + answer);
+
+    }
+
+}
